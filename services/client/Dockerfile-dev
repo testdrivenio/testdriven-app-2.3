3e4d81f03603324a88bc22f83343e28a9c07b32a
Michael Herman <hermanmu@gmail.com>
2018-06-15 22:26:25 -0600

    testing travis

---
+++ services/client/Dockerfile-dev
@@ -0,0 +1,16 @@
+# base image
+FROM node:10.4.1-alpine
+
+# set working directory
+WORKDIR /usr/src/app
+
+# add `/usr/src/app/node_modules/.bin` to $PATH
+ENV PATH /usr/src/app/node_modules/.bin:$PATH
+
+# install and cache app dependencies
+COPY package.json /usr/src/app/package.json
+RUN npm install --silent
+RUN npm install react-scripts@1.1.4 -g --silent
+
+# start app
+CMD ["npm", "start"]
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -9,19 +9,22 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False              # new
+    DEBUG_TB_INTERCEPT_REDIRECTS = False  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True  # new
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
