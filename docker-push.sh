97d6f214747e923244d79235b6bd1a5f09f16ae2
Michael Herman <hermanmu@gmail.com>
2018-06-23 09:56:42 -0600

    testing ecs

--- docker-push.sh
+++ docker-push.sh
@@ -3,6 +3,16 @@
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
 
+  if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
+    export DOCKER_ENV=stage
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-2120066943.us-east-1.elb.amazonaws.com"
+  elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
+    export DOCKER_ENV=prod
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-1112328201.us-east-1.elb.amazonaws.com"
+    export DATABASE_URL="$AWS_RDS_URI"
+    export SECRET_KEY="$PRODUCTION_SECRET_KEY"
+  fi
+
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
@@ -11,16 +21,16 @@ then
     ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-    eval $(aws ecr get-login --region us-west-1 --no-include-email)
+    eval $(aws ecr get-login --region us-east-1 --no-include-email)
     export TAG=$TRAVIS_BRANCH
-    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
+    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
     # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
     # users db
@@ -28,11 +38,11 @@ then
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod $SWAGGER_DIR
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
   fi
---
+++ test-ci.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+env=$1
+fails=""
+
+inspect() {
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
+}
+
+# run client and server-side tests
+dev() {
+  docker-compose -f docker-compose-dev.yml up -d --build
+  docker-compose -f docker-compose-dev.yml run users python manage.py test
+  inspect $? users
+  docker-compose -f docker-compose-dev.yml run users flake8 project
+  inspect $? users-lint
+  docker-compose -f docker-compose-dev.yml run client npm test -- --coverage
+  inspect $? client
+  docker-compose -f docker-compose-dev.yml down
+}
+
+# run e2e tests
+e2e() {
+  docker-compose -f docker-compose-stage.yml up -d --build
+  docker-compose -f docker-compose-stage.yml run users python manage.py recreate_db
+  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  inspect $? e2e
+  docker-compose -f docker-compose-$1.yml down
+}
+
+# run appropriate tests
+if [[ "${env}" == "development" ]]; then
+  echo "Running client and server-side tests!"
+  dev
+elif [[ "${env}" == "staging" ]]; then
+  echo "Running e2e tests!"
+  e2e stage
+elif [[ "${env}" == "production" ]]; then
+  echo "Running e2e tests!"
+  e2e prod
+fi
+
+# return proper code
+if [ -n "${fails}" ]; then
+  echo "Tests failed: ${fails}"
+  exit 1
+else
+  echo "Tests passed!"
+  exit 0
+fi
